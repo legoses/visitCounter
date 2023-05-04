3b66bd8f9f4749b40dec5dadefbd6f01d7afed8c
legoses <stonewal7@outlook.com>
2023-05-04 10:15:09 -0700

    Added license blurb

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 """
+	This program is free software: you can redistribute it and/or modify it under the terms of the 
+    GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
     This is a server to communicate with ESP8266 counters that students bring to workshops
     The ESP8266 chips are low power, and do not support any sort of asymmetrical encryption, but does support AES encryption
     To make sure both sides generate the same key, the esp will first pass the epoch time when it recieves a connection
